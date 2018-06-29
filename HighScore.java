24d39687fe463eebfc9470ebcf6838e5983f0591
Doggie9733 <40678460+Doggie9733@users.noreply.github.com>
2018-06-29 09:47:04 +0800

    Update HighScore.java

--- HighScore.java
+++ HighScore.java
@@ -1,7 +1,28 @@
 import java.io.*;
+import java.io.IOException;
 
 public class HighScore {
-	private String fileName = "HighsScore";
+	private String fileName = "HighScore.txt";
+	private boolean append_to_file = true;
 	
+	
+	public HighScore( String HighScore , boolean append_value ) {
+
+		HighScore = fileName;
+		append_to_file = append_value;
+
+	}
+	
+	public void writeToFile( String textLine ) throws IOException {
+		
+		FileWriter write = new FileWriter( fileName , append_to_file);
+		
+		PrintWriter print_line = new PrintWriter( write );
+		
+		print_line.printf("HighScore");
+		
+		print_line.close();
+		
+	}
 
 }
